c0b458ace7fd4accfb995001300877acce529131
CarollinaAmorim <carollina.amorim@atemporale.it>
2016-05-03 10:50:25 -0300

    pagina serviços quase completa

--- resources/views/public/home/_partial3_whoweare.blade.php
+++ resources/views/public/home/_partial3_whoweare.blade.php
@@ -11,7 +11,7 @@
           <div class="col-lg-8 col-lg-push-1">
             <div class="row">
               <div class="col-md-12">
-                <p class="lead">Digamos que você esteja procurando um escritório de design e comunicação (que bom te ver por aqui!). Ae você se pergunta: &quot;- Por que escolher Atemporale Design?&quot; Bom, somos um estúdio que nasce da imensa vontade de transformar idéias em ações. Acreditamos que um "good design" é aquele que faz sentido e gera oportunidades. Mas além disso procuramos fazer um "design affascinante" que possa influenciar positivamente uma ampla rede social e permanecer no tempo, sendo atemporal(e). Conheça quatro principais razões para nos escolher. 
+                <p class="lead">Digamos que você esteja procurando um escritório de design e comunicação (que bom te ver por aqui!). Ae você se pergunta: &quot;- Por que escolher Atemporale Design?&quot; Bom, somos um estúdio que nasce da imensa vontade de transformar idéias em ações. Acreditamos que um "good design" é aquele que faz sentido e gera oportunidades. Mas além disso procuramos fazer um "design affascinante" que possa influenciar positivamente uma ampla rede social e permanecer no tempo, sendo atemporal(e). Conheça quatro principais razões para nos escolher:
                 </p>
               </div>
             </div>
@@ -22,7 +22,7 @@
                   <div class="info">
                     <h4>Somos responsáveis!
                     </h4>
-                    <p>Responsabilidade é uma das palavras de ordem aqui. Não abrimos mão do que da nosso dever ético e sócio-ambiental. Assim nos nossos projeto inserimos uma "pegada" mais eco. Seja nas soluções, seja no despertar a consciencia dos nossos clientes e parceiros para isso. Adquirindo os nossos serviços você adquire o comprometimento de uma equipe que se preocupa com o nosso planeta.
+                    <p>Sim, nós temos um ideal! Influenciar positivamente nossa sociedade com projetos que tenham uma "pegada" eco, ética e conscienciosa.
                     </p>
                   </div>
                 </div>
@@ -31,9 +31,9 @@
                 <div data-animation-name="fadeInUp" class="icon-box">
                   <div class="icon"><i class="fa fa-trophy"></i></div>
                   <div class="info">
-                    <h4>Somos também Made in Italy!
+                    <h4>Somos made in Italy!
                     </h4>
-                    <p>A idéia de criar Atemporale surgiu na Itália na cidade de Turim. O ambiente de idéias compatíveis e a vontade de deixar a nossa marca no mundo com nossa identidade nos projetos toma forma no Brasil, na capital Belo Horizonte. Trazemos assim, no nosso DNA a tradição, originalidade e &quot;saber fazer&quot; do design italiano com a ousadia, colorido e expontaneidade brasileira.
+                    <p>Trazemos no nosso DNA a herança, originalidade e o <q title="saber fazer">saper fare</q> do design italiano, com a ousadia,  colorido e expontaneidade brasileira.
                     </p>
                   </div>
                 </div>
@@ -44,9 +44,9 @@
                 <div data-animation-name="fadeInUp" class="icon-box">
                   <div class="icon"><i class="fa fa-heart"></i></div>
                   <div class="info">
-                    <h4>Somos sistêmicos!
+                    <h4>Somos criteriosos!
                     </h4>
-                    <p>Entendemos que o todo é maior que a soma de suas partes e o que nos impulsona a estarmos sempre em processo de atualização. Nossa receita é simples: unir as metodologias tradicionais do design com o pensamento sistêmico e o design thinking. Trilhando um caminho próprio que visa encontrar soluções únicas e completas para os nossos clientes. 
+                    <p>As metodologias clássicas do design, o pensamento sistêmico e o design thinking integram a nossa receita para trazer soluções eficazes e atemporais.
                     </p>
                   </div>
                 </div>
@@ -57,7 +57,7 @@
                   <div class="info">
                     <h4>Somos confiáveis!
                     </h4>
-                    <p>Nosso trabalho visa 100% de satisfação dos nosso clientes. Pensando nisso, oferecemos uma garantia exclusiva de alta performance (além das garantias exclusivas personalizadas para cada cliente e pacote) que garante que em até 120 dias do términe de nossos serviços, você vai tranquilamente aumentar em 30% o número de clientes da sua empresa e ainda reforçar as relações com aqueles já existentes. Caso isto não aconteça refazemos, aperfeiçoamos gratuitamente, até conseguir e se ainda assim, por alguma razão não der certo devolvemos o seu dinheiro, sem choro.
+                    <p>Nosso trabalho visa 100% de satisfação dos nossos clientes. Pensando nisso oferecemos a garantia de resultados, ou caso contrário o seu dinheiro de volta!
                     </p>
                   </div>
                 </div>
@@ -72,12 +72,12 @@
     <div class="container">
       <div class="row">
         <div class="col-md-8 col-md-push-4">
-          <h2 class="section-separator-title"><span data-shadow-text="Meet Our Team">Meet Our Team</span></h2>
+          <h2 class="section-separator-title"><span data-shadow-text="Nossa equipe">Conheça nossa equipe</span></h2>
         </div>
       </div>
       <div class="row">
         <div class="col-md-8 col-md-push-4">
-          <p class="lead">Each individual plays a great role in the development of our company. All their contributions are more worth than gold making them an invaluable asset to the company.
+          <p class="lead">Cada um com sua personalidade, experiência e formação integra nossa equipe que trabalha em sintonia para buscar os melhores resultados. Além da nossa equipe fixa contamos ainda com outros colaboradores e parceiros.
           </p>
         </div>
       </div>
@@ -90,12 +90,12 @@
               </figure>
             </div>
             <div class="team-info">
-              <h4 class="team-name">John Hammond
+              <h4 class="team-name">Carollina Amorim
               </h4>
-              <p class="team-role">CEO, Creative Director
+              <p class="team-role">Co-fundadora e Diretora de projetos
               </p>
             </div>
-            <div data-name="John Hammond" data-role="CEO, Creative Director" data-photo="demo/team/1.jpg" class="team-data">
+            <div data-name="Carollina Amorim" data-role="Co-fundadora e Diretora de projetos" data-photo="demo/team/1.jpg" class="team-data">
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
               </p>
               <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
@@ -111,12 +111,12 @@
               </figure>
             </div>
             <div class="team-info">
-              <h4 class="team-name">Dennis Hawkings
+              <h4 class="team-name">Alessandro Traversi
               </h4>
-              <p class="team-role">Lead Developer
+              <p class="team-role">Co-fundador e webdesigner
               </p>
             </div>
-            <div data-name="Dennis Hawkings" data-role="Lead Developer" data-photo="demo/team/2.jpg" class="team-data">
+            <div data-name="Alessandro Traversi" data-role="Lead Developer" data-photo="demo/team/2.jpg" class="team-data">
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
               </p>
               <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
