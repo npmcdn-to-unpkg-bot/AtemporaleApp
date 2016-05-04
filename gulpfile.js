104e8fe66a88c2871e5f23749f2ba651c0c622d7
alessandrotraversi <alessandro.traversi@hotmail.it>
2016-05-04 18:34:15 -0300

    impostado gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var elixir = require('laravel-elixir');
              require('laravel-elixir-jade');
 
+
 elixir.config.sourcemaps = false;
 
 elixir(function(mix) {
@@ -14,10 +15,23 @@ elixir(function(mix) {
     });
 
 
+
+
     mix.less(["resources/assets/less/pages/home.less"], "public/css/home.css");//delete less
 
 
     mix.scripts(["components/layout.js", "pages/home.js"], "public/js/home.min.js");
 
 
+    //icons
+    mix.copy('resources/assets/bower/font-awesome/fonts', 'public/fonts');
+    mix.copy('resources/assets/bower/bootstrap/fonts', 'public/fonts');
+
+
+
+
+    mix.browserSync({
+        proxy: 'http://atemporale_completo.dev/'
+    });
 });
+
